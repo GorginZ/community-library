13b2712436a3bd315ee593f10e08adb0ab447470
Georgia Leng <georgia.z.leng@gmail.com>
2022-10-03 14:05:46 +1100

    basic endppoin

---
+++ src/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type book struct {
+	ISBN   string `json:"isbn"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books = []book{
+	{ISBN: "1", Title: "Brave New World", Author: "Aldous Huxley"},
+	{ISBN: "2", Title: "Das Kapital Volume One", Author: "Karl Marx"},
+	{ISBN: "3", Title: "If This Is A Man", Author: "Primo Levi"},
+	{ISBN: "3", Title: "Sexus", Author: "Henry Miller"},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+
+	router.Run("localhost:8080")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
