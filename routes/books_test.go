635dcb96ba753da0f25dbd8c390712b1ace7d8bb
Georgia Leng <georgia.z.leng@gmail.com>
2022-10-05 14:00:45 +1100

    delete obsolete unhelpful test

--- routes/books_test.go
+++ routes/books_test.go
@@ -5,39 +5,10 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	bookRepository "github.com/GorginZ/community-library/repository"
 	"github.com/gin-gonic/gin"
 )
 
-// fake bs
-var fakeBS *bookRepository.BookService = bookRepository.NewBookService(bookRepository.WithFakeRepository())
-
-// func Test_getBooks(t *testing.T) {
-// 	tests := map[string]struct {
-// 		want    []bookRepository.Book
-// 		wantErr error
-// 	}{
-// 		"getBooks-should-return-books": {
-// 			//TODO  revisit this, it's testing nothing
-// 			want: []bookRepository.Book{
-// 				{ISBN: "1", Title: "Brave New World", Author: "Aldous Huxley"},
-// 				{ISBN: "2", Title: "Das Kapital Volume One", Author: "Karl Marx"},
-// 				{ISBN: "3", Title: "If This Is A Man", Author: "Primo Levi"},
-// 				{ISBN: "3", Title: "Sexus", Author: "Henry Miller"},
-// 			},
-// 			wantErr: nil,
-// 		},
-// 	}
-// 	for name, tt := range tests {
-// 		t.Run(name, func(t *testing.T) {
-// 			b, err := getBooks()
-// 			if b == nil && err != tt.wantErr {
-// 				t.Errorf("got %v, want %v", err, tt.want)
-// 			}
-// 		})
-// 	}
-// }
-
+// whole shebang kind of thing, service inc, so not really just testing books.go
 func Test_Books(t *testing.T) {
 	tests := map[string]struct {
 		wantCode int
