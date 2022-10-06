0dfdb38dea7ed23632d1627a3b422996cbae77b7
Georgia Leng <georgia.z.leng@gmail.com>
2022-10-06 11:58:34 +1100

    remove comments

--- routes/books.go
+++ routes/books.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// how to give this the book service, is this bad? This should probably be on a struct
 var (
 	bookService *br.BookService = br.NewBookService()
 )
@@ -18,16 +17,10 @@ func HandleBooks(c *gin.Context) {
 		c.AbortWithError(500, err)
 		return
 	}
-	//sets content-type as application/json for us
 	c.JSON(http.StatusOK, Books)
 }
 
 func getBooks() ([]br.Book, error) {
 	//TODO err
 	return bookService.BookRepository.GetAll()
-}
-
-// for testing this seems like a bad idea
-func UseServiceWithFakeBookRepository() {
-	bookService = br.NewBookService(br.WithFakeRepository())
 }
