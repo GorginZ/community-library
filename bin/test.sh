fb1ef1c124abc08416fac2ed3a102f3b9289093f
Georgia Leng <georgia.z.leng@gmail.com>
2022-10-07 14:50:05 +1100

    enable testing and linting in ci

--- bin/test.sh
+++ bin/test.sh
@@ -5,6 +5,4 @@ die() { echo "$1" >&2; exit "${2:-1}"; }
 
 hash docker || die "docker not found"
 
-# docker compose run --rm go test -v ./...
-echo "hello testing, uncomment when functional options pr merged"
-
+docker compose run --rm go test -v ./...
